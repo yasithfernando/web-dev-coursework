10bffc63d3bec09e1f3ca6c660040a76c2d130b2
yasithfernando <iyfernando42@gmail.com>
2023-03-12 22:30:16 +0530

    Added  Main Page

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//Import navigation bar from source
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', '../global/navbar.html', true);
+
+xhr.send();
+
+xhr.onload = function(){
+    if (this.status == 200) {
+        document.getElementById('navbar-placeholder').innerHTML = this.responseText;
+    }
+}
+
+
+//Highlight current page on navbar
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current URL
+    var currentURL = window.location.href;
+  
+    // Loop through the navbar links and compare their URLs with the current URL
+    var navbarLinks = document.getElementsByTagName("a");
+    console.log(navbarLinks.length);
+    for (var i = 0; i < navbarLinks.length; i++) {
+      var linkURL = navbarLinks[i].href;
+      console.log(linkURL + " --- " + currentURL);
+      if (currentURL === linkURL) {
+        // Add the "current" class to the corresponding a tag
+        navbarLinks[i].classList.add("current");
+      }
+    }
+  });
