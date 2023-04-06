983ce16e236f28459df285cf4c701ee162f88e07
yasithfernando <iyfernando42@gmail.com>
2023-04-06 13:46:51 +0530

    Final

--- js/footer.js
+++ js/footer.js
@@ -15,3 +15,43 @@ xhrFooter.onload = function(){
     }
   }
 }
+
+function goToEditor(){
+
+  let pages =new Map()
+
+  pages.set('yasith', ["index.html","feedback.html", "page-editor-yasith.html", "extra-content-yasith.html"])
+  pages.set('nethmi', ["buyproducts.html","page-editor-nethmi.html", "extra-content-nethmi.html", "checkout.html"])
+  pages.set('dilana', ["gallery.html", "sitemap.html", "page-editor-dilana.html", "extra-content-dilana.html"])
+  pages.set('chamod', ["quiz.html", "aboutus.html", "end.html","game.html", "highscores.html", "page-editor-chamod.html", "extra-content-chamod.html" ])
+
+  
+
+  // Get the current URL
+  let currentURL = window.location.href;
+  let urlContent = currentURL.split("/")
+  console.log("Current URL: " + urlContent[urlContent.length -1]);
+
+  let newUrl = urlContent[urlContent.length -1]
+
+
+  pages.forEach((value,key) => {
+    value.forEach(page => {
+      if(page == newUrl){
+        let goToLink;
+        if(newUrl == "index.html"){
+          goToLink = "routes/page-editor-" + key + ".html";
+          window.location.href = goToLink;
+        }else{
+          goToLink = "page-editor-" + key + ".html";
+          window.location.href = goToLink;
+        }
+        
+      }
+      
+    })
+  })
+
+
+
+}
